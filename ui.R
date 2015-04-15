cb4d68ddc0e33e1cbd1d5181000bde485cc94262
edisz <eduardszoecs@gmail.com>
2015-04-15 23:12:55 +0200

    slider ranges

--- ui.R
+++ ui.R
@@ -14,16 +14,16 @@ shinyUI(navbarPage("shinytox (alpha)", theme = shinytheme("united"),
                   ),
                    wellPanel(tags$details(tags$summary("Effects"),
                                           sliderInput("muc", HTML("$$\\text{Abundance in Control } (\\mu_C)$$"), value = 10,
-                                                      min = 0.5, max = 200, step = 2),
+                                                      min = 0.5, max = 200, step = 1),
                                           sliderInput('theta', '$$\\text{Dispersion} (\\kappa)$$ ', 
-                                                      value = 4, min = 0.1, max = 200, step = 0.1),
+                                                      value = 4, min = 0.1, max = 250, step = 0.1),
                                           sliderInput('effsize', 'Reduction in treatment (r)',
-                                                      value = 0.5, min = 0, max = 1, step = 0.1)
+                                                      value = 0.5, min = 0, max = 1, step = 0.01)
                                           )
                              ),
                    wellPanel(tags$details(tags$summary("Simulations"),
                                           sliderInput("nsims", "Number of simulations:", 
-                                                      min = 50, max = 250, value = 50, step = 50),
+                                                      min = 50, max = 250, value = 50, step = 10),
                                           textInput('N', 'Sample sizes (separated by comma or space; up to 5 entries): ',
                                                      '3, 6, 9')
                                           )
